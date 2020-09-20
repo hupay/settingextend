830f0061c5c6afe44c743437d7e1783167171a99
胡小兵 <pds_xiaobing_2009@163.com>
2020-09-20 16:04:34 +0800

    增加etcd供应者及测试用例

---
+++ src/SettingExtend.Test/etcdConfigProvider.cs
@@ -0,0 +1,28 @@
+﻿using dotnet_etcd;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SettingExtend.Test
+{
+    public class etcdConfigProvider : IConfiguration
+    {
+        private static string URL = null;
+        static etcdConfigProvider()
+        {
+            URL = Utility.GetConfig()["etcdConfigURL"];
+            if (string.IsNullOrWhiteSpace(URL))
+                throw new SettingException("配置文件地址未设置。");
+        }
+        public EtcdClient etcdClient = null;
+        public etcdConfigProvider()
+        {
+            etcdClient = new EtcdClient(URL);
+        }
+        public string Get(string path)
+        {
+            var response = etcdClient.GetVal(path);
+            return response;
+        }
+    }
+}
