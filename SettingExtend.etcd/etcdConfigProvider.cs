17e7d291f73bb131e1439f8d92dc911e31e34aeb
胡小兵 <liangxuanyi@hotmail.com>
2021-04-07 21:17:49 +0800

    update 增加独立的etcd供应者

--- SettingExtend.etcd/etcdConfigProvider.cs
+++ SettingExtend.etcd/etcdConfigProvider.cs
@@ -1,6 +1,34 @@
-﻿namespace SettingExtend.etcd
+﻿using dotnet_etcd;
+
+namespace SettingExtend.etcd
 {
-    public class etcdConfigProvider
+    /// <summary>
+    /// etcd供应者
+    /// </summary>
+    public class etcdConfigProvider : IConfiguration
     {
+        private static string URL = null;
+        static etcdConfigProvider()
+        {
+            URL = Utility.GetConfig()["etcdConfigURL"];
+            if (string.IsNullOrWhiteSpace(URL))
+                throw new SettingException("配置文件地址未设置。");
+        }
+        public EtcdClient etcdClient = null;
+        public etcdConfigProvider()
+        {
+            etcdClient = new EtcdClient(URL);
+        }
+
+        /// <summary>
+        /// 实现方法
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public string Get(string path)
+        {
+            var response = etcdClient.GetVal(path);
+            return response;
+        }
     }
 }
