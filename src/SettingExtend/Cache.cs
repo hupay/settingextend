b056b08bc0b659080e9a823fa7a2d24489551bde
胡小兵 <liangxuanyi@hotmail.com>
2021-04-20 21:40:24 +0800

    add 缓存

---
+++ src/SettingExtend/Cache.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+
+namespace SettingExtend
+{
+    public static class Cache
+    {
+        private static ConcurrentDictionary<string, string> dictionary = new ConcurrentDictionary<string, string>();
+
+        public static string Get(string key)
+        {
+            string value;
+            if (dictionary.TryGetValue(key, out value))
+                return value;
+            return null;
+        }
+
+        public static void Add(string key,string value)
+        {
+            if(!dictionary.TryAdd(key, value))
+            {
+                dictionary.TryUpdate(key, value, key);
+            }
+        }
+    }
+}
