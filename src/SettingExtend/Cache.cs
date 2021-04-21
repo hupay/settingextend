363d85d5add03297b9447622b02cf40491f7bf96
胡小兵 <liangxuanyi@hotmail.com>
2021-04-21 21:42:51 +0800

    update 增加说明

--- src/SettingExtend/Cache.cs
+++ src/SettingExtend/Cache.cs
@@ -2,10 +2,18 @@
 
 namespace SettingExtend
 {
+    /// <summary>
+    /// 内置缓存
+    /// </summary>
     public static class Cache
     {
         private static ConcurrentDictionary<string, string> dictionary = new ConcurrentDictionary<string, string>();
 
+        /// <summary>
+        /// 得到缓存值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static string Get(string key)
         {
             string value;
@@ -14,6 +22,11 @@ namespace SettingExtend
             return null;
         }
 
+        /// <summary>
+        /// 把新值加入缓存
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         public static void Add(string key,string value)
         {
             if(!dictionary.TryAdd(key, value))
