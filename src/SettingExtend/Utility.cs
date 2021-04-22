96ed477726dde8d1c0ea57d276bf5298caa1c707
胡小兵 <liangxuanyi@hotmail.com>
2021-04-22 21:48:47 +0800

    update 增加日志

--- src/SettingExtend/Utility.cs
+++ src/SettingExtend/Utility.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,15 @@ namespace SettingExtend
     /// </summary>
     public class Utility
     {
+        public static ILogger log = new LoggerFactory().CreateLogger<Utility>();
+
         public static IConfiguration Configuration { get; private set; }
 
         static Utility()
         {
             if (Configuration == null)
             {
+                log.LogDebug("初始化");
                 var config = GetConfig();
                 var type = config["settingextend.provider"];
                 if (string.IsNullOrWhiteSpace(type))
