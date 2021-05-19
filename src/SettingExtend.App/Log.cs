a716a2a8ebf5ba8d5eaac46039982d01b234d1d8
胡小兵 <liangxuanyi@hotmail.com>
2021-05-19 23:17:04 +0800

    add 日志测试

---
+++ src/SettingExtend.App/Log.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingExtend.App
+{
+    public static class Log
+    {
+        public static ILoggerFactory Instance()
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                    .AddConsole();
+            });
+            return loggerFactory;
+        }
+    }
+}
